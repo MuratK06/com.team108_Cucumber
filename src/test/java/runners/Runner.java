02c308b5ff9d635ad75f7d88340bb0c3459ec119
Murat <muratkorkmaz8306@gmail.com>
2023-03-08 12:58:15 +0100

    day14

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,30 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resorurces/features",
+        glue="stepdefinitions",
+        tags = "@wip",
+        dryRun = false
+
+)
+public class Runner {
+
+}
+
+/*
+    Runner class'i testlerimizi tekli veya toplu olarak calistirmamizi saglar
+    Runner class'i BOS bir class'dir
+    burada asil isi yapan 2 tane notasyon
+    ve iclerine yazdigimiz argumentlerdir.
+    @CucumberOptions icerisine yazacagimiz degerler
+    bu Runner class'inin calistiracagi
+        - feature dosyalarinin ve
+        - feature dosyalarina ait Java method'larinin nerede oldugunu
+        - ve bu feature dosyalari icerisinde olusturulan
+          Scenario veya Feature'larin hangilerinin calistirilacagini belirler.
+ */
---
+++ src/test/resorurces/features/US1004_ParametreliUrlTanimlama.feature
@@ -0,0 +1,15 @@
+Feature: US1004 kullanici parametre olarak girdigi Url e gider
+@ilk
+  Scenario: TC8 Kullanici url'i parametre olarak girebilmeli
+
+    Given kullanici "amazonUrl" anasayfaya gider
+    And 3 saniye bekler
+    Then "amazonUrl" sayfasina gittigini test eder
+    And sayfayi kapatir
+
+  Scenario: TC9 Kullanici url'i parametre olarak girebilmeli
+
+    Given kullanici "wiseUrl" anasayfaya gider
+    And 3 saniye bekler
+    Then "wiseUrl" sayfasina gittigini test eder
+    And sayfayi kapatir
